2ca7fd2f86c523fee2de0e6ea8aa5209fdac256c
Jorge Saldana <jojonoob134@gmail.com>
2023-08-24 14:45:12 -0500

    finished variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "james";
+            int number = 96;
+            char singleLetter = 'E';
+            bool stock = true;
+            double bigNumber = 831893.92749;
+            decimal biggerNumber = 7021304.192m;
+
+
+
+            Console.WriteLine($"one an average day for {name} he runs outside and gose for a jog for exactly {number} then gose inside" +
+                $"and sees if his favoret letter, {singleLetter} is in the news, it always is {stock}, then he will say the number {bigNumber} " +
+                $"exactly {biggerNumber}");
         }
     }
 }
